2b7d91f06c018de87f1993c4e94f9312320326bb
gabecodes2 <gabrielcastanheira2002@outlook.com>
2023-11-20 07:31:34 -0500

    UI Colors and Search History added

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// Define constants and get HTML elements
 const apiKey = '5fdd64f174340e34d96c70b1925f05f8';
 const forecastContainer = document.getElementById('forecast-container');
 const todayForecastDetails = document.getElementById('today-forecast-details');
 const searchBar = document.getElementById('search-bar');
 const searchButton = document.getElementById('search-button');
 
+// Function to fetch weather data from OpenWeatherMap API
 async function getWeather(city) {
     try {
         const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`);
@@ -18,6 +20,7 @@ async function getWeather(city) {
     }
 }
 
+// Function to display the forecast on the page
 function displayForecast(data) {
     forecastContainer.innerHTML = '';
     todayForecastDetails.innerHTML = '';
@@ -64,14 +67,47 @@ function displayForecast(data) {
     });
 }
 
+// Function to update the search history
+function updateSearchHistory(city) {
+    const historyList = document.getElementById('history-list');
+    const MAX_HISTORY_ITEMS = 5;
 
+    // Check if the city is already in the search history
+    const isCityInHistory = Array.from(historyList.children).some(item => item.textContent === city);
+
+    if (!isCityInHistory) {
+        // Create a new list item for the search history
+        const listItem = document.createElement('li');
+        listItem.textContent = city;
+
+        // Add click event listener to search with the historical city
+        listItem.addEventListener('click', () => {
+            searchBar.value = city;
+            searchWeather();
+        });
+
+        // Add the new item to the history list
+        historyList.appendChild(listItem);
+
+        // Limit the number of history items displayed
+        if (historyList.children.length > MAX_HISTORY_ITEMS) {
+            historyList.removeChild(historyList.firstChild);
+        }
+    }
+}
+
+// Function to handle the search button click
 function searchWeather() {
     const city = searchBar.value;
     if (city.trim() !== '') {
         getWeather(city)
-            .then(displayForecast)
+            .then(data => {
+                displayForecast(data);
+                updateSearchHistory(city);
+            })
             .catch(error => console.error('Error handling:', error.message));
     }
 }
 
+// Add click event listener to the search button
 searchButton.addEventListener('click', searchWeather);
